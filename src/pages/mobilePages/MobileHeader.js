6f3e3243acfd711c664ef408c28e3b1379d0a7d2
nox590276 <nox590276@naver.com>
2023-08-11 17:07:24 +0900

    아코디언 메뉴 마크업

--- src/pages/mobilePages/MobileHeader.js
+++ src/pages/mobilePages/MobileHeader.js
@@ -1,47 +1,49 @@
-import React from 'react'
-import { IoNotificationsOutline } from "react-icons/io";
+import React from 'react';
 import { TbMenu2 } from "react-icons/tb";
-import { HiOutlineShoppingCart } from "react-icons/Hi"
-import { FaLock } from "react-icons/Fa"
-import { FaUserPlus } from "react-icons/Fa"
-import { HiOutlineHeart } from "react-icons/Hi"
-import { FaUser } from "react-icons/Fa"
+import { HiOutlineShoppingCart } from "react-icons/hi";
+import { FaLock } from "react-icons/fa";
+import { FaUserPlus } from "react-icons/fa";
+import { HiOutlineHeart } from "react-icons/hi";
+import { FaUser } from "react-icons/fa";
+import { MdArrowBack } from "react-icons/md";
+import { MdNotificationsNone } from "react-icons/md";
+import style from './css/mobileHeader.module.css'
 
 export default function MobileHeader() {
   return (
-    <header>
-      <p id='M_noticeIcon'><IoNotificationsOutline/></p>
-      <h2 id='M_logo'>W</h2>
-      <p id='M_shoppingListIcon'><HiOutlineShoppingCart/></p>
-      <p id='M_menuIcon'><TbMenu2/></p>
-      <nav id='M_menu_wrap'>
-        <p id='M_backIcon'></p>
-        <ul id='M_btnList'>
-          <li><p className='btn_icon'><FaLock/></p><p className='btn_title'>Login</p></li>
-          <li><p className='btn_icon'><FaUserPlus/></p><p className='btn_title'>Sign Up</p></li>
-          <li><p className='btn_icon'><HiOutlineHeart/></p><p className='btn_title'>Interest</p></li>
-          <li><p className='btn_icon'><FaUser/></p><p className='btn_title'>My Page</p></li>
+    <header id={style.header}>
+      <h2 id={style.M_logo}>W</h2>
+      <p id={style.M_menuIcon}><TbMenu2 /></p>
+      <p id={style.M_shoppingListIcon}><HiOutlineShoppingCart /></p>
+      <nav id={style.M_menu_wrap}>
+        <div id={style.M_info_wrap}>
+        <p id={style.back_icon}><MdArrowBack /></p>
+        <ul id={style.M_btnList}>
+          <li><p className={style.btn_icon}><FaLock /></p><p className={style.btn_title}>Login</p></li>
+          <li><p className={style.btn_icon}><FaUserPlus /></p><p className={style.btn_title}>Sign Up</p></li>
+          <li><p className={style.btn_icon}><HiOutlineHeart /></p><p className={style.btn_title}>Interest</p></li>
+          <li><p className={style.btn_icon}><FaUser /></p><p className={style.btn_title}>My Page</p></li>
         </ul>
-        <p id='M_sectionBar'> </p>
-        <ul id='M_menuWrap'>
-          <li>Scotch Whisky<span><img src='./images/M_menu_list.svg'/></span>
-          <ul className='M_subMenuWrap'>
-            <li>sub1</li>
-          </ul>
+        </div>
+        <ul id={style.M_menu_wrap}>
+          <li>Scotch Whisky<span><img src='./images/M_menu_list.svg' /></span>
+            <ul className='M_subMenuWrap'>
+              <li>sub1</li>
+            </ul>
           </li>
-          <li>Whisky<span><img src='./images/M_menu_list.svg'/></span>
-          <ul className='M_subMenuWrap'>
-            <li>sub1</li>
-            <li>sub2</li>
-            <li>sub3</li>
-          </ul>
+          <li>Whisky<span><img src='./images/M_menu_list.svg' /></span>
+            <ul className='M_subMenuWrap'>
+              <li>sub1</li>
+              <li>sub2</li>
+              <li>sub3</li>
+            </ul>
           </li>
-          <li>Etc Beverage<span><img src='./images/M_menu_list.svg'/></span>
-          <ul className='M_subMenuWrap'>
-            <li>sub1</li>
-          </ul>
+          <li>Etc Beverage<span><img src='./images/M_menu_list.svg' /></span>
+            <ul className='M_subMenuWrap'>
+              <li>sub1</li>
+            </ul>
           </li>
-          <li>Whisky Search<span><img src='./images/M_menu_list.svg'/></span></li>
+          <li>Whisky Search<span><img src='./images/M_menu_list.svg' /></span></li>
         </ul>
       </nav>
     </header>
--- src/pages/mobilePages/MobileRoot.js
+++ src/pages/mobilePages/MobileRoot.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import MobileHeader from './MobileHeader'
+import MobileHome from './MobileHome'
 
 export default function MobileRoot() {
   return (
-    <div>MobileRoot</div>
+    <div>
+      <MobileHeader/>
+      <MobileHome/>
+    </div>
   )
 }
