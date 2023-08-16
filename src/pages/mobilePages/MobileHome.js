db7156634294527ec058aa8b0d060cb320d62731
nox590276 <nox590276@naver.com>
2023-08-16 15:56:22 +0900

    mobileHome 마크업

--- src/pages/mobilePages/MobileHome.js
+++ src/pages/mobilePages/MobileHome.js
@@ -2,8 +2,92 @@ import React from 'react'
 
 export default function MobileHome() {
   return (
-    <section>
-      MobileHome
+    <section id='mobileHome_wrap'>
+      <div id='M_userBox'>
+        <p id='userImg'></p>
+        <p id='userName'>Login is required</p>
+        <p id='welcomeText'>
+          Find your favorites whisky
+        </p>
+      </div>
+      <ul id='mainVisual_Card'>
+        <li>
+          <p>Must-drink Bourbon Whiskey</p>
+          <p>Bourbon whiskey is renowned for its rich and full-bodied flavor profile.</p>
+        </li>
+        <li>
+          <p>Search for the best whiskey
+            in one place!</p>
+        </li>
+      </ul>
+      <div>
+        <p>TODAY's HIGHLIGHTS</p>
+        <ul>
+          <li>
+            <p>BOWMORE DISTILLERY</p>
+            <p>whisky</p>
+            <p>£2,500.00</p>
+            <img src='' alt='bowmore distillery' />
+          </li>
+          <li>
+            <p>REGALROGUEDARINGDRY</p>
+            <p>whisky</p>
+            <p>€24.50</p>
+            <img src='' alt='regalroguedaringdry' />
+          </li>
+          <li>
+            <p>BOWMORE DISTILLERY7</p>
+            <p>whisky</p>
+            <p>£2,500.00</p>
+            <img src='' alt='bowmore distillery7' />
+          </li>
+          <li>
+            <p>BlUE LABEL</p>
+            <p>whisky</p>
+            <p>£56.95</p>
+            <img src='' alt='blue Label' />
+          </li>
+          <li>
+            <p>BALVENIE</p>
+            <p>whisky</p>
+            <p>£39.95</p>
+            <img src='' alt='bowmore distillery' />
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>RECOMMENDED CONTENTS</p>
+        <ul>
+          <li>
+            <img src='' alt='' />
+            <p>Redbreast 12 Year Old Whiskey</p>
+          </li>
+          <li>
+            <img src='' alt='' />
+            <p>Lagavulin 16 Year Oldwhiskey</p>
+          </li>
+          <li>
+            <img src='' alt='' />
+            <p>Octomore 15 Year Oldwhiskey</p>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <img src='' alt='AMMUNITION STRAIGHT BOURBON FINISHED IN CABERNET SAUVIGNON BARRELS'/>
+            <p>AMMUNITION STRAIGHT BOURBON FINISHED IN CABERNET SAUVIGNON BARRELS</p>
+          </li>
+          <li>
+            <img src='' alt='AMMUNITION STRAIGHT RYE FINISHED IN PINOT NOIR BARRELS'/>
+            <p>AMMUNITION STRAIGHT RYE FINISHED IN PINOT NOIR BARRELS</p>
+          </li>
+          <li>
+            <img src='' alt='HUDSON WHISKEY 5 YEAR NEW YORK STRAIGHT BOURBON'/>
+            <p>HUDSON WHISKEY 5 YEAR NEW YORK STRAIGHT BOURBON</p>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
