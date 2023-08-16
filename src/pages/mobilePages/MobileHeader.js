5697d3ed7055d3349a0793201943229d97e54046
nox590276 <nox590276@naver.com>
2023-08-16 15:14:50 +0900

    모바일 아코디언 메뉴 완성

--- src/pages/mobilePages/MobileHeader.js
+++ src/pages/mobilePages/MobileHeader.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { TbMenu2 } from "react-icons/tb";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import { FaLock } from "react-icons/fa";
@@ -6,33 +6,111 @@ import { FaUserPlus } from "react-icons/fa";
 import { HiOutlineHeart } from "react-icons/hi";
 import { FaUser } from "react-icons/fa";
 import { MdArrowBack } from "react-icons/md";
-import { MdNotificationsNone } from "react-icons/md";
-import style from './css/mobileHeader.module.css'
+// import { MdNotificationsNone } from "react-icons/md";
+import styles from './css/mobileHeader.module.css'
 import { gsap } from 'gsap';
+import { click } from '@testing-library/user-event/dist/click';
 
 export default function MobileHeader() {
-  const M_menu_wrap=useRef()
+  const mainMenuList = [
+    {
+      index: 0, name: 'Scotch Whisky', pathName: '/', subMenuList: [
+        { index: 0, name: 'sub1', pathName: '/' }
+      ]
+    }, {
+      index: 1, name: 'Whisky', pathName: '/', subMenuList: [
+        { index: 0, name: 'sub1', pathName: '/' },
+        { index: 1, name: 'sub2', pathName: '/' },
+        { index: 2, name: 'sub3', pathName: '/' }
+      ]
+    }, {
+      index: 2, name: 'Etc Beverage', pathName: '/', subMenuList: [
+        { index: 0, name: 'sub1', pathName: '/' },
+        { index: 1, name: 'sub2', pathName: '/' }
+      ]
+    }, {
+      index: 3, name: 'Whisky Search', pathName: '/', subMenuList: []
+    }
+  ]
 
-  const activateMenu = ()=>{
-    gsap.to()
-  }
+  const M_menu_wrap = useRef()
+  const backIcon = useRef()
+  const menuIcon = useRef()
+
+  let closeHeight = useMemo(() => (55));
+  let openHeight = useMemo(() => (null));
+
+  const [clickIndex, setClickIndex] = useState(null);
+
+  useEffect(() => {
+    M_menu_wrap.current.style.right = '-100vw';
+    M_menu_wrap.current.style.display = 'none';
+  }, [])
+
+  const activateMenu = useCallback(() => {
+    M_menu_wrap.current.style.display = 'block'
+    gsap.to(M_menu_wrap.current, { right: 0, duration: 0.5, ease: 'power1.out' })
+  }, [])
+  const noneActivateMenu = useCallback(() => {
+    gsap.to(M_menu_wrap.current, {
+      right: -100 + 'vw', duration: 0.5, ease: 'power1.out', onComplete: () => {
+        gsap.set(M_menu_wrap.current, { display: 'none' })
+      }
+    })
+    setClickIndex(null)
+  }, [])
+  const toggleMenu = useCallback((index) => {
+    if (index == clickIndex) {
+      setClickIndex(null)
+    } else {
+      setClickIndex(index)
+    }
+  }, [clickIndex])
   return (
-    <header id={style.M_header}>
-      <h2 id={style.M_logo}>W</h2>
-      <p id={style.M_menuIcon} onClick={activateMenu}><TbMenu2 /></p>
-      <p id={style.M_shoppingListIcon}><HiOutlineShoppingCart /></p>
-      <nav id={style.M_menu_wrap}>
-        <div id={style.M_info_wrap}>
-        <p id={style.back_icon}><MdArrowBack /></p>
-        <ul id={style.M_btnList}>
-          <li><p className={style.btn_icon}><FaLock /></p><p className={style.btn_title}>Login</p></li>
-          <li><p className={style.btn_icon}><FaUserPlus /></p><p className={style.btn_title}>Sign Up</p></li>
-          <li><p className={style.btn_icon}><HiOutlineHeart /></p><p className={style.btn_title}>Interest</p></li>
-          <li><p className={style.btn_icon}><FaUser /></p><p className={style.btn_title}>My Page</p></li>
-        </ul>
+    <header id={styles.M_header}>
+      <h2 id={styles.M_logo}>W</h2>
+      <p id={styles.M_menuIcon} ref={menuIcon} onClick={activateMenu}><TbMenu2 /></p>
+      <p id={styles.M_shoppingListIcon}><HiOutlineShoppingCart /></p>
+      <nav id={styles.M_menu_wrap} ref={M_menu_wrap}>
+        <div id={styles.M_info_wrap}>
+          <p id={styles.back_icon} ref={backIcon} onClick={noneActivateMenu}><MdArrowBack /></p>
+          <ul id={styles.M_btnList}>
+            <li><p className={styles.btn_icon}><FaLock /></p><p className={styles.btn_title}>Login</p></li>
+            <li><p className={styles.btn_icon}><FaUserPlus /></p><p className={styles.btn_title}>Sign Up</p></li>
+            <li><p className={styles.btn_icon}><HiOutlineHeart /></p><p className={styles.btn_title}>Interest</p></li>
+            <li><p className={styles.btn_icon}><FaUser /></p><p className={styles.btn_title}>My Page</p></li>
+          </ul>
         </div>
-        <ul id={style.M_menu_wrap}>
-          <li>Scotch Whisky<span className={style.M_arrowIcon}><img src='./images/M_menu_list.svg' /></span>
+        <ul id={styles.M_menu_wrap}>
+          {mainMenuList.map((item) => (
+            <li key={item.index} className={`${item.index === clickIndex && styles.selected}`}
+              style={item.index === clickIndex ? { height: closeHeight + (closeHeight * item.subMenuList.length)} : { height: closeHeight }}
+              onClick={() => {
+                toggleMenu(item.index)
+              }}>
+
+              {
+                item.subMenuList.length < 1 ?
+                  <>
+                    {item.name}
+                  </>
+                  :
+                  <>
+                    {item.name}
+
+                    <span className={styles.M_arrowIcon}><img src='./images/M_menu_list.svg' alt='' /></span>
+                    <ul className={styles.M_subMenuWrap}>
+                      {
+                        item.subMenuList.map((subItem) => (
+                          <li key={subItem.index}>{subItem.name}</li>
+                        ))
+                      }
+                    </ul>
+                  </>
+              }
+            </li>
+          ))}
+          {/* <li>Scotch Whisky<span className={style.M_arrowIcon}><img src='./images/M_menu_list.svg' /></span>
             <ul className='M_subMenuWrap'>
               <li>sub1</li>
             </ul>
@@ -47,9 +125,10 @@ export default function MobileHeader() {
           <li>Etc Beverage<span className={style.M_arrowIcon}><img src='./images/M_menu_list.svg' /></span>
             <ul className='M_subMenuWrap'>
               <li>sub1</li>
+              <li>sub2</li>
             </ul>
           </li>
-          <li>Whisky Search<span className={style.M_arrowIcon}><img src='./images/M_menu_list.svg' /></span></li>
+          <li>Whisky Search<span className={style.M_arrowIcon}><img src='./images/M_menu_list.svg' /></span></li> */}
         </ul>
       </nav>
     </header>
