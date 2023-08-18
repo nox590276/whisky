5885f3420d9b9dd734d29e7ed1361fbe09e06e91
nox590276 <nox590276@naver.com>
2023-08-18 18:09:23 +0900

    0818 마무리

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,10 @@ import style from './Home/css/Hom.module.css'
 import MainSelectedPage from './Home/MainSelectedPage';
 
 export default function Home() {
-  const [filteredProducts, setFilteredProuducts] = useState([])
   return (
     <div id={style.home_wrap}>
       <MainVisual/>
-      <MainSelectedPage filteredProducts={filteredProducts} />
+      <MainSelectedPage />
     </div>
   )
 }
