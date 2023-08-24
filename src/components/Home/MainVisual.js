ef4405d519fd868eaf04aa2789f897e603d974f3
nox590276 <nox590276@naver.com>
2023-08-24 11:58:15 +0900

    mainVisual코드 파일명 수정

--- src/components/Home/MainVisual.js
+++ src/components/Home/MainVisual.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import MainVisual_textbox from './MainVisualTextbox'
-import MainVisual_whisky_box from './MainVisualWhiskybox'
+import MainVisualTextBox from './MainVisualTextbox'
+import MainVisualWhiskyBox from './MainVisualWhiskybox'
 import style_mainvisual from './css/MainVisual.module.css'
-import style_textbox from './css/MainVisual_textbox.module.css'
+import style_textbox from './css/MainVisualTextbox.module.css'
 import style_whiskybox from './css/MainVisual_whiskybox.module.css'
 
 export default function MainVisual() {
   return (
       <section id={style_mainvisual.mainVisual_wrap}>
-        <div id={style_textbox.mainVisual_textbox_wrap}><MainVisual_textbox /></div>
-        <div id={style_whiskybox.mainVisual_whiskybox_wrap}><MainVisual_whisky_box /></div>
+        <div id={style_textbox.mainVisual_textbox_wrap}><MainVisualTextBox/></div>
+        <div id={style_whiskybox.mainVisual_whiskybox_wrap}><MainVisualWhiskyBox/></div>
         <p id={style_mainvisual.mainVisual_section_bar}></p>
       </section>
   )
